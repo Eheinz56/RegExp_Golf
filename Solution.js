78dbd81313e23645c2b1f6b65f3f69d5a0d678e6
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-03-23 21:50:57 -0400

    Regexp Golf solution. howvever still havent answered the last one yet

---
+++ Solution.js
@@ -0,0 +1,41 @@
+verify(/car|cat/,
+       ["my car", "bad cats"],	
+       ["camper", "high art"]);
+
+verify(/pop|prop/,
+       ["pop culture", "mad props"],
+       ["plop"]);
+
+verify(/ferret|ferry|ferrari/,
+       ["ferret", "ferry", "ferrari"],
+       ["ferrum", "transfer A"]);
+
+verify(/[ious]\b/,
+       ["how delicious", "spacious room"],
+       ["ruinous", "consciousness"]);
+
+verify(/\s(.|,|:|;)/,
+       ["bad punctuation ."],
+       ["escape the dot"]);
+
+verify(/\d{6}/,
+       ["hottentottententen"],
+       ["no", "hotten totten tenten"]);
+
+verify(/.../,
+       ["red platypus", "wobbling nest"],
+       ["earth bed", "learning ape"]);
+
+
+function verify(regexp, yes, no) {
+  // Ignore unfinished exercises
+  if (regexp.source == "...") return;
+  yes.forEach(function(s) {
+    if (!regexp.test(s))
+      print("Failure to match '" + s + "'");
+  });
+  no.forEach(function(s) {
+    if (regexp.test(s))
+      print("Unexpected match for '" + s + "'");
+  });
+}
